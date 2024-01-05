f4ea4b4426253c98caafa4451d60dee40b7a5765
Emisael Kisler <emisael@croney.io>
2024-01-05 07:20:48 -0400

    Agregando clase 10 cadenas.cbl

--- ProgramacionCobol/cadenas.cbl
+++ ProgramacionCobol/cadenas.cbl
@@ -4,21 +4,34 @@
       * Purpose: USO DE CADENAS
       * Tectonics: cobc
       ******************************************************************
-      * La Division IDENTIFICATION DIVISION.
+      * 1) La Division IDENTIFICATION DIVISION.
+      * Division donde identificamos el programa.
        IDENTIFICATION DIVISION.
        PROGRAM-ID. CADENAS-CLASE-10.
-
+      *
+      * 2) Division opcional que describe el entorno de computadora en el que se compila y ejecuta el programa.
        ENVIRONMENT DIVISION.
-
+       SOURCE-COMPUTER. HP-G3-800
+      *
+      * 3) En la Division de datos definimos las variables que utilizaremos en el programa.
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
        77  WS-CADENA         PIC IS XXXXX.
        77  WS-CADENA_ALFAB   PIC A(30).
        77  WS-CADENA-ALFAN   PIC X(30).
-
+       77  WS-CADENA-NUMERIC PIC 9(5).
+      *
+      * 4) La Division de procedimiento es donde escribimos la logica del programa
+      * Ademas es la responsable de llevar a cabo las tareas de procesamiento de datos codificadas en el programa.
        PROCEDURE DIVISION.
        PROGRAM-BEGIN.
+           DISPLAY "Introduce cualquier cadnea de numeros:".
+           ACCEPT WS-CADENA-NUMERIC.
+           DISPLAY "Cadena de numeros es: " WS-CADENA-NUMERIC.
+           DISPLAY "Longitud: "FUNCTION LENGTH(WS-CADENA-NUMERIC).
+           DISPLAY "-----------------------------------------"
+           
            DISPLAY "Introduce cualquier cadena de texto:".
            ACCEPT WS-CADENA.
            DISPLAY "Cualquier cadena de texto es: " WS-CADENA.
